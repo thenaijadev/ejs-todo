842945e98cfcf3c3ef68844dd6e47609039e1102
Okwor Chukwuemeka <thenaijadev@gmail.com>
2021-12-20 23:02:59 -0800

    update with database

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const {
+    isBuffer
+} = require("util");
 
 const app = express();
 
@@ -11,32 +15,81 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-const newItems = ["Buy Food", "Cook Food", "Eat Food"];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-const  workItems = [];
+const itemSchema = {
+    name: String,
+}
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Buy food"
+});
+
+
+const item2 = new Item({
+    name: "cook food"
+});
+
+
+const item3 = new Item({
+    name: "Eat food"
+});
+
+const defaultItems = [item1, item2, item3];
+
 
 
 app.get("/", (req, res) => {
-    const day = date.getDate ()
 
-    res.render("list", {
-        listTitle: day,
-        newItems: newItems
+    Item.find({}, function (err, foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfull");
+                }
+            });
+        } else {
+            res.render("list", {
+                listTitle: "Toaday",
+                newItems: foundItems
+
+            });
+        }
     });
+
+
+
 });
+
+
 
 app.post("/", (req, res) => {
 
-   const newItem = req.body.newItem;
+    const newItem = req.body.newItem;
 
-    newItems.push(newItem);
+    const item = new Item({
+        name: newItem,
+    });
+
+    item.save();
     res.redirect("/");
-
-
-
     // The code above points to the get route in order to grab the variable "newitem"
 
 
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (!err) {
+            console.log("Delete successsfull");
+            res.redirect("/");
+        }
+    })
 });
 
 app.get("/work", (req, res) => {
